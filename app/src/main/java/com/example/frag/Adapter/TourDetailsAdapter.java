09c2749bde10cef7490a186a2ef12a193496d0d4
nhan <nhan280101a@gmail.com>
2022-11-21 19:07:21 +0700

    20/11/22

---
+++ app/src/main/java/com/example/frag/Adapter/TourDetailsAdapter.java
@@ -0,0 +1,58 @@
+package com.example.frag.Adapter;
+
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.frag.R;
+
+import java.util.List;
+
+public class TourDetailsAdapter extends RecyclerView.Adapter<TourDetailsAdapter.PhotoViewHoder> {
+    private List<TourPhoto> mlistPhoto;
+
+    public TourDetailsAdapter(List<TourPhoto> mlistPhoto) {
+        this.mlistPhoto = mlistPhoto;
+    }
+
+    @NonNull
+    @Override
+    public PhotoViewHoder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tourdetails,parent,false);
+
+
+        return new PhotoViewHoder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PhotoViewHoder holder, int position) {
+        TourPhoto tourPhoto = mlistPhoto.get(position);
+        if(tourPhoto == null){
+            return;
+        }
+        holder.imgPhoto.setImageResource(tourPhoto.getResourceID());
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mlistPhoto != null){
+            return mlistPhoto.size();
+        }
+        return 0;
+    }
+
+    public class PhotoViewHoder extends RecyclerView.ViewHolder {
+
+        private ImageView imgPhoto;
+
+        public PhotoViewHoder(@NonNull View itemView) {
+            super(itemView);
+            imgPhoto = itemView.findViewById(R.id.imgTourDetails);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/frag/Adapter/TourPhoto.java
@@ -0,0 +1,17 @@
+package com.example.frag.Adapter;
+
+public class TourPhoto {
+    private int resourceID;
+
+    public int getResourceID() {
+        return resourceID;
+    }
+
+    public void setResourceID(int resourceID) {
+        this.resourceID = resourceID;
+    }
+
+    public TourPhoto(int resourceID) {
+        this.resourceID = resourceID;
+    }
+}
